824f949ea6a62c9e5fbc79c52d33db94cb5a9299
EarlGrae <31806865+EarlGrae@users.noreply.github.com>
2019-11-08 18:57:17 -0800

    Add files via upload

--- Explorer.py
+++ Explorer.py
@@ -1,7 +1,7 @@
-import sys
+import sys, os, re
+from threading import Thread
 from PyQt5.QtWidgets import *
 from PyQt5.QtCore import pyqtSlot
-
 
 class App(QMainWindow):
 
@@ -11,37 +11,79 @@ class App(QMainWindow):
         self.left = 10
         self.top = 10
         self.width = 680
-        self.height = 80
+        self.height = 660
+        self.all_drives = self.get_drives()
         self.initUI()
 
     def initUI(self):
+        self.all_drives = self.get_drives()
+
         self.setWindowTitle(self.title)
         self.setGeometry(self.left, self.top, self.width, self.height)
 
         self.button = QPushButton('Search', self)
         self.button.move(550, 25)
 
+        self.combo = QComboBox(self)
+        self.combo.move(20, 80)
+
+        self.button2 = QPushButton('Go', self)
+        self.button2.move(150, 80)
+
         # Create textbox
         self.textbox = QLineEdit(self)
         self.textbox.move(20, 20)
         self.textbox.resize(500, 40)
-        self.textbox.returnPressed.connect(self.button.click)
 
-        # connect button to function on_click
+        self.response_box = QLineEdit(self)
+        self.response_box.move(20, 120)
+        self.response_box.resize(640, 500)
+
+        self.textbox.returnPressed.connect(self.button.click)
+        self.combo.activated.connect(self.button2.click)
+        self.button2.clicked.connect(self.get_text)
         self.button.clicked.connect(self.on_click)
+
         self.show()
+
+    @pyqtSlot()
+    def get_text(self):
+        self.response_box.setText('')
+        chosen_path = self.combo.currentText()
+        self.combo.clear()
+        self.response_box.setText(chosen_path)
 
     @pyqtSlot()
     def on_click(self):
         textboxValue = self.textbox.text()
-        self.display(textboxValue)
-        # display rn will remove all records in all folders w/o a keyword, so not final product
+        li = self.search_directory(textboxValue)
+        self.combo.addItems(li)
         self.textbox.setText("")
 
-    def display(self, wordSearch):
-        dialog = QFileDialog()
-        dialog.setNameFilter("*{}*".format(wordSearch))
-        dialog.exec()
+    @staticmethod
+    def get_drives():
+        response = os.popen("wmic logicaldisk get caption")
+        list1 = []
+        for line in response.readlines():
+            line = line.strip("\n")
+            line = line.strip("\r")
+            line = line.strip(" ")
+            if (line == "Caption" or line == ""):
+                continue
+            list1.append(line)
+        print(drive for drive in list1)
+        return list1
+
+    def search_directory(self, keyword):
+        results = []
+        for each in self.all_drives:
+            for root, dir, files in os.walk(each, topdown=True):
+                for f in files:
+                    if os.path.splitext(f)[1] == '.txt':
+                        if re.compile(keyword).search(os.path.join(root, f)):
+                            results.append(os.path.join(root, f))
+        return results
+
 
 
 
